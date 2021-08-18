9bd46c2a4f8bf465f9c49eff84cd49aba4900024
Danny C PC <dannyc52521@gmail.com>
2021-08-18 20:39:04 +1000

    Api working, adding filter data func

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,25 @@
 /******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.js":
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
-const app = (function() {
+/***/ (() => {
 
-  const grabWeather = async function() {
-    
-  }
-})()
+throw new Error("Module parse failed: Unexpected token (27:12)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|   const filterData = function(data) {\n|     const filtered = {\n>         data.name,\n|         data.weather[0].main,\n|         data.weather[0].description, ");
 
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.js"]();
+/******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0E7QUFDQTtBQUNBLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9hcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgYXBwID0gKGZ1bmN0aW9uKCkge1xuXG4gIGNvbnN0IGdyYWJXZWF0aGVyID0gYXN5bmMgZnVuY3Rpb24oKSB7XG4gICAgXG4gIH1cbn0pKClcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7O1VDQUE7VUFDQTtVQUNBO1VBQ0E7VUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYXRoZXItYXBwL3dlYnBhY2svYmVmb3JlLXN0YXJ0dXAiLCJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvd2VicGFjay9zdGFydHVwIiwid2VicGFjazovL3dlYXRoZXItYXBwL3dlYnBhY2svYWZ0ZXItc3RhcnR1cCJdLCJzb3VyY2VzQ29udGVudCI6WyIiLCIvLyBzdGFydHVwXG4vLyBMb2FkIGVudHJ5IG1vZHVsZSBhbmQgcmV0dXJuIGV4cG9ydHNcbi8vIFRoaXMgZW50cnkgbW9kdWxlIGRvZXNuJ3QgdGVsbCBhYm91dCBpdCdzIHRvcC1sZXZlbCBkZWNsYXJhdGlvbnMgc28gaXQgY2FuJ3QgYmUgaW5saW5lZFxudmFyIF9fd2VicGFja19leHBvcnRzX18gPSB7fTtcbl9fd2VicGFja19tb2R1bGVzX19bXCIuL3NyYy9hcHAuanNcIl0oKTtcbiIsIiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/app.js
+++ src/app.js
@@ -1,6 +1,50 @@
 const app = (function() {
 
-  const grabWeather = async function() {
-    
-  }
+const grabData = async function (city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2b1b01c9401096e78d474e286123aa8a`;
+    try {
+      const response = await fetch(url, {mode: "cors"});
+      const data = await response.json();
+      console.log(data);
+      // return console.log(
+      //   data.name,
+      //   data.weather[0].main,
+      //   data.weather[0].description, 
+      //   data.main.temp,
+      //   data.main.feels_like,
+      //   data.wind.speed,
+      //   data.sys.sunrise,
+      //   data.sys.sunset
+      //   );
+      
+    } catch (error) {
+      console.log(error);
+    }
+  } 
+
+  const filterData = function(data) {
+    const filtered = {
+        data.name,
+        data.weather[0].main,
+        data.weather[0].description, 
+        data.main.temp,
+        data.main.feels_like,
+        data.wind.speed,
+        data.sys.sunrise,
+        data.sys.sunset
+    }
+    }
+  
+
+  // grabWeather().catch((err) => {
+  //   console.log(err)
+  // })
+
+  return { grabData }
 })()
+
+app.grabData('London');
+
+
+
+
